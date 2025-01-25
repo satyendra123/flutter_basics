a2d5ec1956a881aa6f4f56aee6b5d053157b1f03
satyendra123 <Singhsatyendra885@gmail.com>
2025-01-25 05:49:38 +0530

    Update example_1.dart

--- example_1.dart
+++ example_1.dart
@@ -27,5 +27,20 @@ Text(
   this is similar like this in html and css 
   <p style=" font-size: 24px; font-weight: bold; color: blue;letter-spacing: 2px;text-align: center;white-space: nowrap;overflow: hidden;text-overflow: ellipsis;transform: scale(1.2);">Hello World!</p>
 
-  Note-2 aur agar mai is text ko center karna chahu horizontally aur vertically to mai flutter me center widget ka use karunga
+  Note-2 aur agar mai is text ko center karna chahu horizontally aur vertically to mai flutter me center widget ka use karunga. jaise agar mujhe ye same kaam css me karna hota to mai kaise karta. mai simply ek parent div banata
+  aur us parent div ke andar apne p tag ko rakhta aur parent div me stype lgata display:flex, justify content center and align items center. similarly in this we will put all the code in the parent which is called the center widgets
+
+    import 'package:flutter/material.dart';
+
+void main() {
+  runApp(
+    const Center(
+      child: Text(
+        'Hello World',
+        textDirection: TextDirection.ltr,
+      ),
+    ),
+  );
+}
+
   
